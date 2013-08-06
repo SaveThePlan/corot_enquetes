701dfbb860ba01658a99e808dea6dbdab068068c
realnanook <>
2013-08-06 16:30:36 +0200

    creation dossiers Entity et Mapper

---
+++ module/Application/src/Application/Entity/Enquete.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Enquete
+ *
+ * @author stagiaire
+ */
+class Enquete {
+    //put your code here
+}
+
+?>
---
+++ module/Application/src/Application/Mapper/EnqueteMapper.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EnqueteMapper
+ *
+ * @author stagiaire
+ */
+class EnqueteMapper {
+    //put your code here
+}
+
+?>
