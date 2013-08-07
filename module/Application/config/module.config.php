954e4cbf16d405d8b76e9a46cdd89b754dec1d4c
Jimmy <>
2013-08-07 14:58:01 +0200

    Ajout de zfc-user et config

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -20,6 +20,16 @@ return array(
                     ),
                 ),
             ),
+            'zfcuser' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/user',
+                    'defaults' => array(
+                        'controller' => '\Application\Controller\User',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
