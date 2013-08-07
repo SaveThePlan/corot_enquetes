fdb8c8b8e55d29077b11fe87c86a32e19e51e5e0
realnanook <realnanook@gmail.com>
2013-08-07 14:32:24 +0200

    création des actions (vides) dans les deux controleurs Public et Membre

--- module/Application/src/Application/Controller/PublicController.php
+++ module/Application/src/Application/Controller/PublicController.php
@@ -2,7 +2,33 @@
 
 namespace Application\Controller;
 
-class PublicController
-{
-    //put your code here
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class PublicController extends AbstractActionController {
+
+    /**
+     *
+     * @var Request
+     */
+    protected $request;
+
+    public function indexAction() {
+        //todo page accueil (inscription et connexion)
+        return new ViewModel(
+                array(
+                //'enquetes' => $enquetes 
+                )
+        );
+    }
+
+    public function repondreAction() {
+        //todo action répondre à un questionnaire...
+        return new ViewModel(
+                array(
+                //'enquetes' => $enquetes 
+                )
+        );
+    }
+
 }
