4531d1f8f419ec3d70f99b20da3cd1f236b9b2de
issam/franck <>
2013-08-07 14:54:30 +0200

    vues sources

---
+++ module/Application/view/application/compte.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+    <form method="POST" action="">
+        <table>
+            <tr><th>nom</th><td><input type="text" name="name" value="danaus"/></td></tr>
+            <tr><th>prénom</th><td><input type="text" name="firstname" value="franck"/></td></tr>
+            <tr><th>email</th><td><input type="email" name="email" value="franck@gmail.com"/></td></tr>
+            <tr><th>mot de passe</th><td><input type="password" name="password" value="gfdgfdsklg"/></td></tr>     
+            
+        </table>
+        
+        <input type="submit" value="valider modification" name="valider" />
+        
+    </form>
+        
