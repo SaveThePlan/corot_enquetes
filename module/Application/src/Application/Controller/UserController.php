d3c822f57d1a5b1583e22ea7667f41e85ea34041
Jimmy <>
2013-08-07 12:23:12 +0200

    Ajout UserController

---
+++ module/Application/src/Application/Controller/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserController
+ *
+ * @author stagiaire
+ */
+
+namespace Application\Controller;
+
+use Zend\View\Model\ViewModel;
+
+class UserController extends \ZfcUser\Controller\UserController
+{
+
+    public function indexAction()
+    {
+        if (!$this->zfcUserAuthentication()->hasIdentity()) {
+            return $this->redirect()->toRoute(static::ROUTE_LOGIN);
+        }
+        //$tableGateway = $this->getServiceLocator()->get('AlbumTableGateway');
+        $adapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+        $mapperEnquete = new EnqueteMapper($adapter);
+        
+        $enquetes = $mapperEnquete->getAllByUser($this->zfcUserAuthentication()->getIdentity()->getId());
+        
+        return new ViewModel(
+                array(
+            //'enquetes' => $enquetes 
+            )
+               );
+    }
+
+}
