44b68fe7f71810aa0cd8fabf9e3735eef22edc73
realnanook <realnanook@gmail.com>
2013-08-08 11:28:42 +0200

    alter SQL

--- _SQL/alter_sondages_struct_1.sql
+++ _SQL/alter_sondages_struct_1.sql
@@ -1,3 +1,5 @@
 USE sondages;
 
 ALTER TABLE `sondages`.`user` CHANGE COLUMN `password` `password` VARCHAR(128) NOT NULL  ;
+
+ALTER TABLE `sondages`.`reponse` CHANGE COLUMN `uid_repondant` `uid_repondant` VARCHAR(255) NOT NULL  ;
