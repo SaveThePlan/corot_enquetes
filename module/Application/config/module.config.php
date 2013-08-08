cb15b651a64a1d4c4d3e246f44b9a7f3e2dac08c
realnanook <realnanook@gmail.com>
2013-08-08 07:31:28 +0200

    Après confirmation de Romain, suppression des '.html' inutiles à la fin des url

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,14 +15,14 @@ return array(
              * Les Routes inquisitor
              * ---------------------
              * home             = ACCUEIL - public/index                 : / ()
-             * repondreEnquete  = RÉPONDANT - public/repondre            : /enquete_##.html (id enquete)
-             * listeEnquetes    = MEMBRE - membre/index                  : /liste-enquetes.html (no param : id membre géré par zfcUser)
-             * creerEnquete     = CRÉER ENQUÊTE - membre/creer           : /creer-enquete.html ()
-             * voirEnquete      = VOIR ENQUÊTE - membre/apercu           : /apercu-enquete_##.html (id enquete)
-             * modifierEnquete  = MODIFIER ENQUÊTE - membre/modifier     : /editer-enquete_##.html (id enquete)
-             * supprimerEnquete = SUPPRIMER ENQUÊTE - membre/supprimer   : /supprimer-enquete_##.html (id enquete)
-             * resultatsEnquete = RÉSULTATS - membre/consulter           : /resultats-enquete_##.html (id enquete)
-             * effacerDonnees   = EFFACER DONNÉES - membre/effacerData   : /effacer-donnees_##.html (id enquete)
+             * repondreEnquete  = RÉPONDANT - public/repondre            : /enquete_## (id enquete)
+             * listeEnquetes    = MEMBRE - membre/index                  : /liste-enquetes (no param : id membre géré par zfcUser)
+             * creerEnquete     = CRÉER ENQUÊTE - membre/creer           : /creer-enquete ()
+             * voirEnquete      = VOIR ENQUÊTE - membre/apercu           : /apercu-enquete_## (id enquete)
+             * modifierEnquete  = MODIFIER ENQUÊTE - membre/modifier     : /editer-enquete_## (id enquete)
+             * supprimerEnquete = SUPPRIMER ENQUÊTE - membre/supprimer   : /supprimer-enquete_## (id enquete)
+             * resultatsEnquete = RÉSULTATS - membre/consulter           : /resultats-enquete_## (id enquete)
+             * effacerDonnees   = EFFACER DONNÉES - membre/effacerData   : /effacer-donnees_## (id enquete)
              */
             
             /**
@@ -47,47 +47,46 @@ return array(
             ),
 
             /* repondreEnquete : vers les enquêtes déstinées aux répondants 
-             * url : /enquete_##.html  ---> url simple qui s'adresse à un public large (lors de la diffusion)
+             * url : /enquete_##  ---> url simple qui s'adresse à un public large (lors de la diffusion)
              * param : id de l'enquete
              */
             'repondreEnquete' => array(
                 'type' => 'Regex',
                 'options' => array(
                     //extracteur
-                    'regex' => '/enquete_(?<id>[1-9][0-9]*)\.html', 
+                    'regex' => '/enquete_(?<id>[1-9][0-9]*)', 
                     'defaults' => array(
                         'controller' => 'Application\Controller\Public',
                         'action' => 'repondre',
                     ),
                     //generateur
-                    'spec' => '/enquete_%id%.html',
+                    'spec' => '/enquete_%id%',
                 ),
             ),
 
             /* listeEnquetes : vers la page 'membre', la liste des enquêtes du membre 
-             * url : /liste-enquetes_##.html
-             * param : id du membre
+             * url : /liste-enquetes
+             * no param : id du membre géré directement par zfcuser
              */
             'listeEnquetes' => array(
-                'type' => 'Regex',
+                'type' => 'Literal',
                 'options' => array(
-                    'regex' => '/liste-enquetes_(?<id>[1-9][0-9]*)\.html', 
+                    'route' => '/liste-enquetes', 
                     'defaults' => array(
                         'controller' => 'Application\Controller\Membre',
                         'action' => 'index',
                     ),
-                    'spec' => '/liste-enquetes_%id%.html',
                 ),
             ),
 
             /* creerEnquete : vers la page de création d'une enquête
-             * url : /creer-enquete.html
+             * url : /creer-enquete
              * param : rien c'est une page statique
              */
             'creerEnquete' => array(
                 'type' => 'Literal',
                 'options' => array(
-                    'route' => '/creer-enquete.html', 
+                    'route' => '/creer-enquete', 
                     'defaults' => array(
                         'controller' => 'Application\Controller\Membre',
                         'action' => 'creer',
@@ -101,79 +100,79 @@ return array(
             'modifierEnquete' => array(
                 'type' => 'Regex',
                 'options' => array(
-                    'regex' => '/editer-enquete_(?<id>[1-9][0-9]*)\.html', 
+                    'regex' => '/editer-enquete_(?<id>[1-9][0-9]*)', 
                     'defaults' => array(
                         'controller' => 'Application\Controller\Membre',
                         'action' => 'modifier',
                     ),
-                    'spec' => '/editer-enquete_%id%.html',
+                    'spec' => '/editer-enquete_%id%',
                 ),
             ),
 
             /* supprimerEnquete : vers la page de suppression d'une enquête (avec confirmation)
-             * url : /supprimer-enquete_##.html
+             * url : /supprimer-enquete_##
              * param : id de l'enquête
              */
             'supprimerEnquete' => array(
                 'type' => 'Regex',
                 'options' => array(
-                    'regex' => '/supprimer-enquete_(?<id>[1-9][0-9]*)\.html',
+                    'regex' => '/supprimer-enquete_(?<id>[1-9][0-9]*)',
                     'defaults' => array(
                         'controller' => 'Application\Controller\Membre',
                         'action' => 'supprimer',
                     ),
-                    'spec' => '/supprimer-enquete_%id%.html',
+                    'spec' => '/supprimer-enquete_%id%',
                 ),
             ),
 
             /* voirEnquete : vers l'aperçu d'une enquête
-             * url : /apercu-enquete_##.html
+             * url : /apercu-enquete_##
              * param : id de l'enquête
              */
             'voirEnquete' => array(
                 'type' => 'Regex',
                 'options' => array(
-                    'regex' => '/apercu-enquete_(?<id>[1-9][0-9]*)\.html',
+                    'regex' => '/apercu-enquete_(?<id>[1-9][0-9]*)',
                     'defaults' => array(
                         'controller' => 'Application\Controller\Membre',
                         'action' => 'apercu',
                     ),
-                    'spec' => '/apercu-enquete_%id%.html',
+                    'spec' => '/apercu-enquete_%id%',
                 ),
             ),
 
             /* resultatsEnquete : vers la page des résultats d'une enquête
-             * url : /resultats-enquete_##.html
+             * url : /resultats-enquete_##
              * param : id de l'enquête
              */
             'resultatsEnquete' => array(
                 'type' => 'Regex',
                 'options' => array(
                     //analyse de mon url
-                    'regex' => '/resultats-enquete_(?<id>[1-9][0-9]*)\.html', 
+                    'regex' => '/resultats-enquete_(?<id>[1-9][0-9]*)', 
                     'defaults' => array(
                         'controller' => 'Application\Controller\Membre',
                         'action' => 'consulter',
                     ),
-                    'spec' => '/resultats-enquete_%id%.html',
+                    'spec' => '/resultats-enquete_%id%',
                 ),
             ), 
             
 
             /* effacerDonnees : vers la page des résultats d'une enquête
-             * url : /effacer-donnees_##.html
+             * url : /effacer-donnees_##
              * param : id de l'enquête
              */
             'effacerDonnees' => array(
                 'type' => 'Regex',
                 'options' => array(
                     //analyse de mon url
-                    'regex' => '/effacer-donnees_(?<id>[1-9][0-9]*)\.html', 
+                    'regex' => '/effacer-donnees_(?<id>[1-9][0-9]*)', 
                     'defaults' => array(
                         'controller' => 'Application\Controller\Membre',
                         'action' => 'effacer',
                     ),
-                    'spec' => '/effacer-donnees_%id%.html',
+                    'spec' => '/effacer-donnees_%id%',
                 ),
             ), 
             
