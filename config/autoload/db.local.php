ef7b43992169327bd753d7c1215947a82bd73222
Jimmy <>
2013-08-08 21:47:02 +0200

    Ignore

--- config/autoload/db.local.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-        'db' => array(
-        'username' => 'root', //user de la db
-        'password' => '', // mdp de la db (vide si wamp|xamp par défaut sur windows)
-    )
-);
