626c65679cbaa81ffe8646acfb486f9f5b499681
realnanook <realnanook@gmail.com>
2013-08-08 17:21:16 +0200

    wip creer enquete

--- module/Application/src/Application/Controller/MembreController.php
+++ module/Application/src/Application/Controller/MembreController.php
@@ -21,39 +21,46 @@ use Application\Mapper\QuestionMapper;
 use Zend\View\Model\ViewModel;
 use ZfcUser\Controller\UserController;
 
-class MembreController extends UserController
-{
+class MembreController extends UserController {
 
     /**
      * index membre = liste enquêtes
      * 
      * @return ViewModel
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         /* authentification user */
         $this->userAuth();
-        
+
         $adapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
         $mapperEnquete = new EnqueteMapper($adapter);
 
         /* création de la liste d'enquêtes */
         $enquetes = $mapperEnquete->getAllByIdUser(1); // pour test
 //        $enquetes = $mapperEnquete->getAllByIdUser($this->zfcUserAuthentication()->getIdentity()->getId());
-        
-        if(!$enquetes) {
+
+        if (!$enquetes) {
             $this->flashMessenger()->addInfoMessage("Vous n'avez pas encore créé d'enquête.");
         }
 
         return new ViewModel(
                 array(
-                'enquetes' => $enquetes 
+            'enquetes' => $enquetes
                 )
         );
     }
-    
-    public function creerAction()
-    {
+
+    public function creerAction() {
+        $formCreation = new \Application\Form\CreerForm();
+
+        return new ViewModel(
+                array(
+            'formCreation' => $formCreation
+                )
+        );
+    }
+
+    public function modifierAction() {
         return new ViewModel(
                 array(
                 //'enquetes' => $enquetes 
@@ -61,9 +68,7 @@ class MembreController extends UserController
         );
     }
 
-    
-    public function modifierAction()
-    {
+    public function supprimerAction() {
         return new ViewModel(
                 array(
                 //'enquetes' => $enquetes 
@@ -71,66 +76,50 @@ class MembreController extends UserController
         );
     }
 
-    
-    public function supprimerAction()
-    {
-        return new ViewModel(
-                array(
-                //'enquetes' => $enquetes 
-                )
-        );
-    }
-
-    
-    public function apercuAction()
-    {
+    public function apercuAction() {
         $this->userAuth();
         // TODO sécurité : comment tester si l'enquête demandée appartient bien à l'utilisateur...
         // peut-être en passant par une liste d'enquêtes stckées dans le user ?
-
         //recupère le paramètre get de l'url
-        $idEnquete = (int)$this->params('id');
+        $idEnquete = (int) $this->params('id');
         $adapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
         $mapper = new EnqueteMapper($adapter);
-        
+
         $enquete = $mapper->getById($idEnquete);
-        
-        if(!$enquete) {
+
+        if (!$enquete) {
             $this->flashMessenger()->addErrorMessage("L'enquête demandée est inaccessible !");
             return $this->redirect()->toRoute('listeEnquetes');
         }
-        
+
         //enquete ok : récupération de la liste des questions
         $mapperQuestion = new QuestionMapper($adapter);
         $listeQuestions = $mapperQuestion->getAllByIdEnquete($idEnquete);
-        
-        if($listeQuestions) {
+
+        if ($listeQuestions) {
             $enquete->setListeQuestions($listeQuestions);
         }
-        
-        
+
+
         $formEnquete = new \Application\Form\EnqueteForm($enquete->getListeQuestions(), $adapter);
-                
+
         return new ViewModel(
                 array(
-                'enquete' => $enquete,
-                'formEnquete' => $formEnquete
+            'enquete' => $enquete,
+            'formEnquete' => $formEnquete
                 )
         );
     }
 
-    
-    public function consulterAction()
-    {
+    public function consulterAction() {
         return new ViewModel(
                 array(
                 //'enquetes' => $enquetes 
                 )
         );
     }
-    
-    public function effacerAction()
-    {
+
+    public function effacerAction() {
         return new ViewModel(
                 array(
                 //'enquetes' => $enquetes 
@@ -144,16 +133,14 @@ class MembreController extends UserController
      * 
      * @return void
      */
-    private function userAuth()
-    {
+    private function userAuth() {
         /* redirection vers page de login si user inconnu... */
         // desactivé pour le moment car pénible de se connecter à chaque fois...
 //        if (!$this->zfcUserAuthentication()->hasIdentity()) {
 //            return $this->redirect()->toRoute(static::ROUTE_LOGIN);
 //        }
-        
+
         return;
     }
-    
-    
+
 }
