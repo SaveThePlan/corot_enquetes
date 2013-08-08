15dfe322c772bcc2b79493ac302d5f2f4b29f26b
bioub <>
2013-08-08 15:06:32 +0200

    fichier bd.local.php pour windows

--- config/autoload/db.local.php
+++ config/autoload/db.local.php
@@ -2,6 +2,6 @@
 return array(
         'db' => array(
         'username' => 'root', //user de la db
-        'password' => 'root', // mdp de la db (vide si wamp|xamp par défaut sur windows)
+        'password' => '', // mdp de la db (vide si wamp|xamp par défaut sur windows)
     )
 );
